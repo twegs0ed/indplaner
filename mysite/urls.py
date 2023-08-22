4e93142c861029b35f77e97666649597d14542a1
Иванов Владимир Владимирович <ivanov_vv@cpt-pskov.ru>
2023-08-22 15:15:11 +0300

    title

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,8 +2,8 @@
 from django.contrib import admin
 from django.urls import include,path
 
-admin.site.site_header = 'Учет инструмента'
-admin.site.site_title = 'Учет инструмента'
+admin.site.site_header = 'Учет готовых деталей АО "ЦПТ"'
+admin.site.site_title = 'Учет готовых деталей АО "ЦПТ"'
 admin.site.index_title = ''
 urlpatterns = [
     #path('', include('tools.urls')),
