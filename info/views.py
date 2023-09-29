02f4afd7c094de096d1017d39d79d84c265ac968
Иванов Владимир Владимирович <ivanov_vv@cpt-pskov.ru>
2023-09-29 11:17:51 +0300

    1

--- info/views.py
+++ info/views.py
@@ -3,18 +3,19 @@ from .forms import SearchtoolForm
 from django.http import HttpResponseRedirect
 from tools.models import Toolsonwarehouse, Tools, Priem
 from order.models import Order
+from work.models import Work
 
 
 def info(request):
     if request.GET.get('tool'):
         form = SearchtoolForm(request.GET)
         result = request.GET.get('tool')
-        print(result)
         tools=Toolsonwarehouse.objects.filter(title__icontains  = result.upper()).all()
         toolsv=Tools.objects.filter(tool__title__icontains  = result.upper())
         priems = Priem.objects.filter(tool__title__icontains  = result.upper())
         orders = Order.objects.filter(tool__title__icontains  = result.upper())
-        return render(request, 'info.html', {'tools':tools, 'toolsv':toolsv, 'priems':priems, 'orders':orders, 'form':form})
+        works = Work.objects.filter(tool__title__icontains  = result.upper())
+        return render(request, 'info.html', {'tools':tools, 'toolsv':toolsv, 'priems':priems, 'orders':orders, 'works':works, 'form':form})
     form = SearchtoolForm()
     return render(request, 'info.html', { 'form':form})
 
