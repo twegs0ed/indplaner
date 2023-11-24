1cef78c57ecc6b7e97789646ec3c9358870877ae
twegs0ed <tweg@bk.ru>
2023-11-25 01:08:24 +0300

    gantt

--- info/urls.py
+++ info/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.info, name='info_info'),
+    path('gantt', views.gantt, name='info_gantt'),
     #path('<int:pk>/', views.tool_detail, name='tool_detail'),
 ] 
--- info/views.py
+++ info/views.py
@@ -5,6 +5,8 @@ from tools.models import Toolsonwarehouse, Tools, Priem
 from order.models import Order
 from work.models import Work
 from django.contrib.admin.models import LogEntry, ADDITION, CHANGE, DELETION
+from datetime import datetime 
+from django.utils.dateformat import format
 
 
 def info(request):
@@ -19,5 +21,50 @@ def info(request):
         return render(request, 'info.html', {'tools':tools, 'toolsv':toolsv, 'priems':priems, 'orders':orders, 'works':works, 'form':form})
     form = SearchtoolForm()
     return render(request, 'info.html', { 'form':form})
+
+
+def gantt(request):
+    if request.GET.get('tool'):
+        form = SearchtoolForm(request.GET)
+        projects = Order.objects.filter(firm__title__icontains  = request.GET.get('tool')).all()
+    else:
+        projects = Order.objects.filter(firm__title__icontains  = '').order_by('exp_date').all()
+    form = SearchtoolForm()
+    
+    
+
+    
+    data = [{
+		'id': "1",
+		'name': request.GET.get('tool'),
+        
+		'actualStart':int(round(projects[0].order_date_worker.timestamp() * 1000)),
+		'actualEnd': int(round(datetime.timestamp(datetime.combine(projects[len(projects)-1].exp_date, datetime.min.time())))*1000)  ,
+		'children': [
+			
+
+		]
+	}]
+    prew_id=0
+    for p in projects:
+        if p.exp_date:
+            d=int(round(datetime.timestamp(datetime.combine(p.exp_date, datetime.min.time())))*1000) 
+        else: d=1700164761000
+        prc={
+				'id': p.id,
+				'name': p.tool.title,
+				'actualStart':int(round(p.order_date_worker.timestamp() * 1000)),
+                
+		        'actualEnd': d,
+				'connectTo': prew_id,
+				'connectorType': "finish-start",
+				'progressValue': "75%"
+			}
+        prew_id=p.id
+        data[0]['children'].append(prc)
+    
+    
+        
+    return render(request, 'gantt.html', { 'data':data, 'form':form})
 
 
