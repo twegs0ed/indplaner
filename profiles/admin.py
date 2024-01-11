e261eafbdd0695f6f8bce8289035701d79643c73
Иванов Владимир Владимирович <ivanov_vv@cpt-pskov.ru>
2024-01-11 10:50:13 +0300

    machines

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Profile, StanProfile, Operation, Machine
+from import_export.admin import ExportActionMixin, ImportExportModelAdmin
+from import_export import resources
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('bio','pk')
     ordering = ['bio']
@@ -12,9 +14,36 @@ def get_machines(obj):
         t=t+machine.name+' | '
     return t
 get_machines.short_description='Станки'
+def get_username(obj):
+    
+    return obj.user.first_name+' '+obj.user.last_name
+get_username.short_description='ФИО'
 
-class StanProfileAdmin(admin.ModelAdmin):
-    list_display = ('user','operation', get_machines, 'pk')
+
+
+
+class StanProfileResource(resources.ModelResource):
+    
+    class Meta:
+        model = StanProfile
+
+        fields = ('user__first_name', 'user__last_name', 'machines')
+        #export_order = ('tool', 'count')
+        exclude = ('id',)
+        #import_id_fields = ('tool', 'count')
+    def dehydrate_machines(self, obj):
+        t=''
+        for machine in obj.machines.all():
+            t=t+machine.name+' ; '
+        return t 
+
+
+
+
+
+class StanProfileAdmin(ExportActionMixin, admin.ModelAdmin):
+    resource_class = StanProfileResource
+    list_display = ('user', get_username, 'operation',  get_machines, 'pk')
     list_editable = ['operation']
     autocomplete_fields = ['user','operation', 'machines']
     search_fields = ['user__first_name','user__last_name', 'user__username','operation__name',]
