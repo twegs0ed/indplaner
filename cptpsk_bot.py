0ea201735c37c11a0c3a106a0cb54493dfbb9e72
Иванов Владимир Владимирович <ivanov_vv@cpt-pskov.ru>
2024-02-02 14:02:16 +0300

    bot

--- cptpsk_bot.py
+++ cptpsk_bot.py
@@ -2,6 +2,7 @@ import mysite.asgi
 import datetime
 import telebot;
 from order.models import Order
+from work.models import Work
 bot = telebot.TeleBot('6532461428:AAFo13Xq6NAsy8y5JTlDJQsWFIy6HZE4IWA');
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
@@ -17,6 +18,18 @@ def get_text_messages(message):
             t=str(order.tool.title)+'-'+str(order.count)+'шт., запущено '+str(order.exp_date)
             t+='. '+'на складе '+str(order.tool.count)
             bot.send_message(message.from_user.id, t)
+            bot.send_message(message.from_user.id, 'Изготовление:')
+            
+            works=Work.objects.filter(tool__title__contains=message.text)
+            for work in works:
+                t=''
+                t+=str(work.user.first_name)+' '+str(work.user.last_name)+' - '+str(work.date)+'. Станки: '
+                for  m in work.user.stanprofile.machines.all:
+                    t+=str(m.name)
+                t+='. '+str(work.count)
+                bot.send_message(message.from_user.id, t)
+                    
+
     else:
         bot.send_message(message.from_user.id, "Не найдено")
 
