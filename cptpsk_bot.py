0cca12d3b7c20589a5294b52c3f45ccbf25c8c07
Иванов Владимир Владимирович <ivanov_vv@cpt-pskov.ru>
2024-04-08 15:57:46 +0300

    '1'

--- cptpsk_bot.py
+++ cptpsk_bot.py
@@ -6,22 +6,19 @@ from work.models import Work
 bot = telebot.TeleBot('6532461428:AAFo13Xq6NAsy8y5JTlDJQsWFIy6HZE4IWA');
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    '''if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")'''
+    
     orders=Order.objects.filter(tool__title__contains=message.text.upper()).order_by('-order_date_worker')[:10]
+    bot.send_message(message.from_user.id, "Запуск:") 
     if orders:
         for order in orders:
             t='*'+str(order.tool.title)+'*'+'-'+str(order.count)+'шт., запущено '+str(order.exp_date)
             t+='. '+'на складе '+str(order.tool.count)+'('+str(order.tool.workplace)+')'
+            t+=' Изделие:'+order.firm.title+'.\n'
             bot.send_message(message.from_user.id, t, parse_mode="Markdown")
     else:
-        bot.send_message(message.from_user.id, "Не найдено")        
+        bot.send_message(message.from_user.id, "Не найдено запуска")        
             
-    works=Work.objects.filter(tool__title__contains=message.text.upper()).order_by('-date')[:15]
+    works=Work.objects.filter(tool__title__contains=message.text.upper()).order_by('-date')[:8]
     if works:
         bot.send_message(message.from_user.id, 'Изготовление:')
         for work in works:
